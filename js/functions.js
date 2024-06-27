2adbe2ad8363adbbeec3e00f50d9cf091e847839
Dmitriu <dimriudobrunin@gmail.com>
2024-06-27 10:28:28 +0300

    Создаст Функцию для проверки, является ли строка палиндромом

--- js/functions.js
+++ js/functions.js
@@ -12,3 +12,24 @@ checkLengthToString('проверяемая строка', 20);
 // checkLengthToString('проверяемая строка', 18);
 // checkLengthToString('проверяемая строка', 10);
 
+// Функция для проверки, является ли строка палиндромом;
+const isPalindrome = function (string) {
+  let changeString = string.replaceAll(' ', '');
+  changeString = changeString.toLowerCase();
+
+  let workString = [];
+  for (let i = changeString.length - 1; i >= 0; i--) {
+    workString = workString + changeString[i];
+  }
+
+  if (changeString === workString) {
+    return true;
+  } else {
+    return false;
+  }
+
+};
+
+isPalindrome('Лёша на полке клопа нашёл ');
+// isPalindrome('ДовОд');
+// isPalindrome('Кекс');
